907f8fe063a46e2ead1c30377e7b635ee8075950
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-05-06 00:36:50 +0700

    Add main

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Button, Container } from 'react-bootstrap'
 import "../styles/main.css"
+import Lego from '../assets/lego/bitmap.png'
 
 const Main = () => {
    return (
-      <div className="Main">
-         <Container>
-            aw
-         </Container>
-      </div>
-   )
+     <div className="Main text-center">
+       <Container>
+         <h1>WEEKEND FROM HOME</h1>
+         <a>Stay active with a little workout.</a>
+         <div className="LegoButton">
+            <img src={Lego} />
+            <div className="Button">Let's Go</div>
+         </div>
+       </Container>
+     </div>
+   );
 }
 
 export default Main
