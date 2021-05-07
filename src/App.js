8486ce702edf885876a3e602b9c08a2797fdce6e
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-05-07 22:16:28 +0700

    add Testimonial

--- src/App.js
+++ src/App.js
@@ -9,15 +9,33 @@ import Definition from './pages/Definition';
 
 
 function App() {
+  const testimonial = [
+    {
+      title: "Blu Kicks",
+      content:
+        "Places where you can leverage tools and software to free up time to focus on growing the business.",
+    },
+    {
+      title: "Angelus",
+      content:
+        "All those apps took me months to get running. Now the site practically runs itself!",
+    },
+    {
+      title: "SoYoung",
+      content:
+        "Unless you have a truly unique product, it will be very hard to differentiate and gain brand traction",
+    },
+  ];
   return (
     <div className="App">
       <Header />
       <Main />
       <Definition />
-      {/* <Submain
+      <Testimonial title="TESTIMONIAL" content={testimonial} />
+      <Submain
         title="POV"
         content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo nesciunt quaerat sequi perferendis facere corrupti odio temporibus consequuntur, modi sapiente autem obcaecati quam vel ratione ea dolorem ut explicabo laudantium."
-      /> */}
+      />
       {/* <Footer /> */}
     </div>
   );
--- src/pages/Testimonial.jsx
+++ src/pages/Testimonial.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
+import { Card, Container } from 'react-bootstrap';
 import "../styles/testimonial.css"
 
-const Testimonial = () => {
+const Testimonial = (props) => {
+   const testimonial = props.content
    return (
-      <div className="Testimonial">
-         TESTIMONIAL
-      </div>
-   )
+     <div className="Testimonial text-center">
+       <div className="Title">{props.title}</div>
+       <div className="Content">
+          <Container className="d-flex">
+            {testimonial.map((listTestimonial) => (
+               <Card className="bg-white border-0 pb-5">
+                  <Card.Body className="text-left pt-2">
+                     <Card.Title
+                     className="ContentTitle"
+                     style={{
+                        whiteSpace: "nowrap",
+                        textOverflow: "ellipsis",
+                        overflow: "hidden",
+                        }}
+                        >
+                     {listTestimonial.title}
+                     </Card.Title>
+                     <Card.Text className="ContentContent">
+                        {listTestimonial.content}
+                     </Card.Text>
+                  </Card.Body>
+               </Card>
+            ))}
+         </Container>
+       </div>
+     </div>
+   );
 }
 
 export default Testimonial
