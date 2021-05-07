b7f88567a93119bd59bea5329ccff38cb908c7b8
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-05-07 23:18:55 +0700

    add scroll fungction

--- src/pages/Testimonial.jsx
+++ src/pages/Testimonial.jsx
@@ -1,33 +1,40 @@
 import React from 'react'
-import { Card, Container } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import "../styles/testimonial.css"
 
 const Testimonial = (props) => {
    const testimonial = props.content
+
+   const scrollLeft = () => {
+      document.getElementById("Container").scrollLeft += 300;
+   }
+
+   const scrollRight = () => {
+      document.getElementById("Container").scrollLeft -= 300;
+   }
+
+
    return (
      <div className="Testimonial text-center">
        <div className="Title">{props.title}</div>
        <div className="Content">
-          <Container className="d-flex">
-            {testimonial.map((listTestimonial) => (
+         <Container fluid className="d-flex p-0">
+           <Col onClick={scrollLeft}>KIRI</Col>
+           <Col md={6} id="Container" className="d-flex overflow-auto">
+             {testimonial.map((listTestimonial) => (
                <Card className="bg-white border-0 pb-5">
-                  <Card.Body className="text-left pt-2">
-                     <Card.Title
-                     className="ContentTitle"
-                     style={{
-                        whiteSpace: "nowrap",
-                        textOverflow: "ellipsis",
-                        overflow: "hidden",
-                        }}
-                        >
+                 <Card.Body className="text-left pt-2">
+                   <Card.Title className="ContentTitle">
                      {listTestimonial.title}
-                     </Card.Title>
-                     <Card.Text className="ContentContent">
-                        {listTestimonial.content}
-                     </Card.Text>
-                  </Card.Body>
+                   </Card.Title>
+                   <Card.Text className="ContentContent">
+                     {listTestimonial.content}
+                   </Card.Text>
+                 </Card.Body>
                </Card>
-            ))}
+             ))}
+           </Col>
+           <Col onClick={scrollRight}>KANAN</Col>
          </Container>
        </div>
      </div>
