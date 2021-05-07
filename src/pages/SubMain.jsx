9e983d5ac71d8df06f758ca0dbecefdee8eacb23
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-05-07 22:27:20 +0700

    restyle Submain

--- src/pages/SubMain.jsx
+++ src/pages/SubMain.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import { Col } from 'react-bootstrap'
 import "../styles/submain.css"
 
 const Submain = (props) => {
    return (
       <div className="Submain">
-         <div className="Title">
-            {props.title}
-         </div>
-         <div className="Content">
-            {props.content}
-         </div>
+         <Col md={12}>
+            <div className="Title">
+               {props.title}
+            </div>
+            <div className="Content">
+               {props.content}
+            </div>
+         </Col>
       </div>
    ) 
 }
